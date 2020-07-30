c7a2a04071990b200d16ee83b02a8d37f9465a49
Sergio <chejo.lopez2015.cl@gmail.com>
2020-07-29 21:38:16 -0600

    Categoria

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ from userObj import UserObj
 from inversorLogic import inversorLogic
 from inversorObj import inversorObj
 from emprendedorLogic import emprendedorLogic
+from categoriaLogic import CategoriaLogic
 
 app = Flask(__name__)
 app.secret_key = "ILoveFishing"
@@ -39,9 +40,47 @@ def emprendimiento():
     return render_template("emprendimiento.html")
 
 
-@app.route("/categoria")
+@app.route("/categoria", methods=["GET", "POST"])
 def categoria():
-    return render_template("categoria.html")
+    logic = CategoriaLogic()
+    massage = ""
+    verdadero = False
+    if request.method == "GET":
+        data = logic.getAllCategorias()
+        return render_template("categoria.html", data=data, massage=massage)
+    elif request.method == "POST":
+        formId = int(request.form["formId"])
+        # Inserta una categoría
+        if formId == 1:
+            categoria = request.form["categoria"]
+            logic.insertCategoria(categoria)
+            massage = "Se ha insertado un nuevo usuario"
+            data = logic.getAllCategorias()
+            return render_template("categoria.html", data=data, massage=massage)
+        # Elimina una categoria
+        elif formId == 2:
+            id = int(request.form["id"])
+            logic.deleteCategoria(id)
+            massage = "Se ha eliminado un usuario"
+            data = logic.getAllCategorias()
+            return render_template("categoria.html", data=data, massage=massage)
+        # Va al form para dar update
+        elif formId == 3:
+            id = int(request.form["id"])
+            categoria = request.form["categoria"]
+            verdadero = True
+            data = logic.getAllCategorias()
+            return render_template(
+                "categoria.html",
+                data=data,
+                verdadero=verdadero,
+                categoria=categoria,
+                id=id,
+            )
+        # Modifica una categoria
+        else:
+            massage = "Se ha insertado un nuevo usuario"
+            return render_template("categoria.html", data=data, massage=massage)
 
 
 if __name__ == "__main__":
